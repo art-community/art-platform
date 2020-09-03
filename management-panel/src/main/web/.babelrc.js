d5764fa828002909fda39ee9eae2f9ff17a2e3c2
anton.bashirov <anton.sh.local@gmail.com>
2020-09-04 00:33:14 +0300

    upgrade

--- management-panel/src/main/web/.babelrc.js
+++ management-panel/src/main/web/.babelrc.js
@@ -16,6 +16,19 @@ const plugins = [
             'camel2DashComponentName': false
         },
         'icons'
+    ],
+    [
+        'babel-plugin-transform-imports',
+        {
+            '@material-ui/core': {
+                'transform': '@material-ui/core/esm/${member}',
+                'preventFullImport': true
+            },
+            '@material-ui/icons': {
+                'transform': '@material-ui/icons/esm/${member}',
+                'preventFullImport': true
+            }
+        }
     ]
 ];
 
